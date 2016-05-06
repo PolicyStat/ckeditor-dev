55ea84a315e4688679d85ee527bd9b2fb0592ce4
Alex Tsai <caffodian@gmail.com>
2016-05-05 22:51:40 -0700

    tests for the different paste plugin.

---
+++ tests/plugins/shift_enter_paste_into_list/extrapastefilter.js
@@ -0,0 +1,42 @@
+/* bender-tags: editor,unit */
+/* bender-ckeditor-plugins: toolbar,clipboard,pastetext,shift_enter_paste_into_list */
+/* bender-include: ../clipboard/_helpers/pasting.js */
+/* global assertPasteEvent */
+
+( function() {
+	'use strict';
+	bender.editor = {
+		config: {
+			enterMode: CKEDITOR.ENTER_P,
+			shiftEnterMode: CKEDITOR.ENTER_BR
+		}
+	};
+
+	var tests = {};
+
+	tests.setUp = function() {
+		CKEDITOR.plugins.clipboard.resetDragDataTransfer();
+		// be inside a list item
+		bender.tools.setHtmlWithSelection( this.editor, '<ol><li>^</li></ol>' );
+	};
+
+	tests[ 'test_single_p_get_removed_in_list_item' ] = function() {
+		var editor = this.editor,
+			pasteContent = '<p>asdf</p>',
+			expectedContent = 'asdf';
+
+		assertPasteEvent( editor, { dataValue: pasteContent },
+			{ dataValue: expectedContent }, 'removed p tag' );
+	};
+
+	tests[ 'test_multiple_p_becomes_break_in_list_item' ] = function() {
+		var editor = this.editor,
+			pasteContent = '<p>asdf</p><p>jkl</p>',
+			expectedContent = 'asdf<br />jkl';
+
+		assertPasteEvent( editor, { dataValue: pasteContent },
+			{ dataValue: expectedContent }, 'removed p tag' );
+	};
+
+	bender.test( tests );
+}() );
